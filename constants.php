89cf0e673fe97fab292b96d5c8159246fee6fc7d
Abdelrahman Naser <abdonaser_97@outlook.com>
2018-05-12 00:15:27 +0200

    Create constants.php

---
+++ constants.php
@@ -0,0 +1,9 @@
+<?php
+define('TYPE', 'mysql');
+define('HOST', 'localhost');
+define('DB', 'task');
+define('DB_CHARSET', 'UTF8');
+define('USER', 'root');
+define('PASS', '');
+define('PDO_DSN', TYPE.':host=' . HOST . ';dbname=' . DB . ';charset=' . DB_CHARSET);
+?>
